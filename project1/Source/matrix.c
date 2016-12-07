09a1cb78c1801ab3ac92f339ef62efe5ffe518e3
Zihan Zheng <zzh1996@mail.ustc.edu.cn>
2016-12-07 08:54:23 +0800

    finish project1

--- project1/Source/matrix.c
+++ project1/Source/matrix.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<time.h>
 
 #define INF 1000000000
 
@@ -24,26 +25,39 @@ void matrix_chain_order(int p[],int n){
     }
 }
 
-void print_optimal_parens(int i,int j){
+void print_optimal_parens(FILE *f,int i,int j){
     if(i==j)
-        printf("A%d",i);
+        fprintf(f,"A%d",i);
     else{
-        printf("(");
-        print_optimal_parens(i,s[i][j]);
-        print_optimal_parens(s[i][j]+1,j);
-        printf(")");
+        fprintf(f,"(");
+        print_optimal_parens(f,i,s[i][j]);
+        print_optimal_parens(f,s[i][j]+1,j);
+        fprintf(f,")");
     }
 }
 
 int main(){
-    FILE *f=fopen("../Input/input1.txt","r");
-    int data[30]={30,35,15,5,10,20,25};
+    FILE *f,*t;
+    int data[30];
     int i;
+    double diff;
+    struct timespec start,stop;
+    f=fopen("../Input/input1.txt","r");
     for(i=0;i<30;i++){
-        //fscanf(f,"%d",&data[i]);
+        fscanf(f,"%d",&data[i]);
     }
     fclose(f);
-    matrix_chain_order(data,6);
-    print_optimal_parens(1,6);
+    f=fopen("../Output/output.txt","w");
+    t=fopen("../Output/time.txt","w");
+    for(i=4;i<=28;i+=4){
+        clock_gettime(CLOCK_MONOTONIC,&start); //开始计时
+        matrix_chain_order(data,i);
+        clock_gettime(CLOCK_MONOTONIC,&stop); //结束计时
+        diff=(stop.tv_sec-start.tv_sec)+(double)(stop.tv_nsec-start.tv_nsec)/1000000000L;
+        fprintf(t,"%d %.9fs\n",i,diff);
+        print_optimal_parens(f,1,i);
+        fprintf(f,"\n");
+    }
+    fclose(f);
     return 0;
 }
